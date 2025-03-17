c7d3a30d1bbafd7584fcf7f9ae421a51e3d633dc
johnmagusrecords <dj.johnmagus@gmail.com>
2025-03-18 01:39:26 +0400

    Add config.py 1742247564820

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration settings
+API_KEY = "ZhNNE4pnHX1Ku67d"
+
+API_SECRET = "@boutonY27"  # Keeping the same password
+
+
+BASE_URL = "https://demo-api-capital.backend-capital.com/api/v1"
